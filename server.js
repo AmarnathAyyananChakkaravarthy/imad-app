e6ab0f6458a24d8e083988c87f56a7670c278168
AmarnathAyyananChakkaravarthy <33284585+AmarnathAyyananChakkaravarthy@users.noreply.github.com>
2018-03-20 18:25:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    articleone : {
+        title:'Article-One|Amarnath',
+        heading:'Article-One',
+        date:'20Mar2018',
+        content:`Hello Everone There! This IS Article OnE Of This WebPage!Hello Everone There! This IS Article OnE Of     ThisWebPage!HelloEverone There! This IS Article OnE Of This WebPage!Hello Everone There! This IS Article OnE Of This  WebPage!`
+    },
+    
+    articletwo : {
+        title:'Article-Two|Amarnath',
+        heading:'Article-Two',
+        date:'20Mar2018',
+        content:`Hello Everone There! This IS Article Two Of This WebPage!`
+    }
+};
+
+function createtemp(d){
+    var title=d.title;
+    var heading=d.heading;
+    var date=d.date;
+    var content=d.content;
+    var htmlcontent=`<!DOCTYPE html>
+    <html>
+        <head>
+            <title>
+                 ${titile}
+            </title>
+            <link href="/ui/style.css" rel="stylesheet"/>
+            <meta name="viewport" content="width-device-width,initial-scale=1" /> 
+         </head>
+         <body>
+            <div class="container" >
+                <div>
+                    <a href='/'>home </a>
+                </div>
+                
+                <div>
+                    <h2>
+                        ${heading}
+                    </h2>
+                    <hr/>
+                    <h3>
+                        ${date}
+                    </h3>
+                    
+                </div>
+                <div>
+                    <p>
+                        ${content}
+                    </p>
+                </div>
+            </div>
+        </body>
+    </html>
+    `    
+    return htmlcontent;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articlename', function (req, res) {
+  res.send(htmltemp(atricles[articlename]));
 });
 
 app.get('/ui/style.css', function (req, res) {
